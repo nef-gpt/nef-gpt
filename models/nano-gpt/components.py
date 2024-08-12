63e316ea1b6771414a3779416355d99848c18160
Luca Fanselau <luca.fanselau@outlook.com>
2024-08-12 12:47:38 +0200

    Move nano gpt

--- models/nano-gpt/components.py
+++ models/nano-gpt/components.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import math
 
 
 class LayerNorm(nn.Module):
--- nef-gpt/__init__.py
+++ nef-gpt/__init__.py
@@ -1,11 +1,1 @@
 # Start the experiment
-
-from experiments import *
-
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
